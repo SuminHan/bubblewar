53b1c7044ce76f184083ddf95273c7d1331b4246
goldmoonkaist <shan28@ucsc.edu>
2015-08-09 22:56:48 -0700

    team fight available

--- basicserver/app.js
+++ basicserver/app.js
@@ -1,17 +1,35 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 user = {};
+teamA = 0;
+teamB = 0;
+pushA = 0;
+pushB = 0;
 
 app.get('/', function(req, res){
 	//res.send('<h1>Hello world</h1>');
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static(__dirname));
+
 io.on('connection', function(socket){
 	if(!user[socket.id]){
-		user[socket.id] = 0;
+		if(teamA>teamB){
+			user[socket.id] = {n:0, team:'B'};
+			teamB += 1;
+			io.emit('myteam', 'B');
+		}
+		else{
+			user[socket.id] = {n:0, team:'A'};
+			teamA += 1;
+			io.emit('myteam', 'A');
+		}
+		io.emit('pushedbutton', user);
+		io.emit('status', {A:pushA, B:pushB});
 	}
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
@@ -20,10 +38,16 @@ io.on('connection', function(socket){
 		console.log('message: ' + msg);
 	});
 	socket.on('pushbutton', function(msg){
-		user[socket.id] += 1;
+		user[socket.id].n += 1;
+		if(user[socket.id].team == 'A')	
+			pushA += 1;
+		else
+			pushB += 1;
+
 		console.log(user);
 		console.log('pushed button from ' + socket.id);
 		io.emit('pushedbutton', user);
+		io.emit('status', {A:pushA, B:pushB});
 	});
 	io.emit('some event', { for: 'everyone' });
 });
