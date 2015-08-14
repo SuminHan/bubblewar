9ba611daaaeda7d682f4910f416a3ab330173ce1
goldmoonkaist <shan28@ucsc.edu>
2015-08-13 23:10:40 -0700

    20150813 Space --> New Circle (keypress)

--- basicserver/page.js
+++ basicserver/page.js
@@ -1,7 +1,50 @@
-Physics(function(world){
 
-  var viewWidth = 500;
-  var viewHeight = 300;
+$( document ).ready(function() {
+  console.log( "ready!" );
+  var socket = io();
+  var myteam;
+  $('#btn').click(function( event ) {
+    socket.emit('pushbutton', '');
+  });
+
+  $(document).on('keypress', function( event ) {
+    
+    var key = event.which || event.keyCode;
+    if(key == '32')
+      socket.emit('pushbutton', $('#m').val());
+  });
+
+  socket.on('pushedbutton', function(msg){
+    world.add(
+      Physics.body('circle', {
+        x: 0, // x-coordinate
+        y: 0, // y-coordinate
+        vx: 0.1, // velocity in x-direction
+        vy: 0.1, // velocity in y-direction
+        radius: 10
+      }) );
+
+    console.log(msg);
+    $('#messages').html('')
+    for(var k in msg){
+      $('#messages').append($('<li>').text(k.substring(0, 10) + ' ====' + msg[k].team +'==== ' + msg[k].n)); }
+  });
+  socket.on('myteam', function(msg){
+    myteam = msg;
+    if(myteam == 'A')
+      $('#btn').removeClass('btn-default').addClass('btn-danger');
+    else
+      $('#btn').removeClass('btn-default').addClass('btn-info');
+  });
+
+  socket.on('status', function(msg){
+    console.log(msg);
+    $('#txt').text('teamA: ' + msg.A + ' vs. teamB: ' + msg.B);
+  });
+
+  var world = Physics();
+  var viewWidth = 1000;
+  var viewHeight = 600;
   var x = document.getElementById("viewport");
 
   var renderer = Physics.renderer('canvas', {
@@ -38,17 +81,9 @@ Physics(function(world){
     vy: 0.01, // velocity in y-direction
     radius: 15
   });
-  var ball3 = Physics.body('circle', {
-    x: 250, // x-coordinate
-    y: 30, // y-coordinate
-    vx: 0.2, // velocity in x-direction
-    vy: -0.4, // velocity in y-direction
-    radius: 30
-  });
   // add the circle to the world
   world.add( ball1 );
   world.add( ball2 );
-  world.add( ball3 );
 
   // render on each step
   world.on('step', function(){
